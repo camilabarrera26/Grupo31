8d9955306232d243b253dd9ef12f20690b2c8fdc
CAMILA BARRERA <camila.barrera@uc.cl>
2021-05-24 19:48:12 -0400

    'web3'

--- Sites/consultas/consulta_tiendas_cobertura.php
+++ Sites/consultas/consulta_tiendas_cobertura.php
@@ -6,11 +6,7 @@
   require("../config/conexion.php");
 
   #Se construye la consulta como un string
- 	$query = "SELECT tiendas.nombre, comunas.comuna_cobertura 
-     FROM comunas, direccionesdespacho, tiendas
-     WHERE comunas.did = direccionesdespacho.did AND
-     direccionesdespacho.tid = tiendas.tid;
-     ";
+ 	$query = "SELECT tiendas.nombre, comunas.comuna_cobertura FROM comunas, direccionesdespacho, tiendas WHERE comunas.did = direccionesdespacho.did AND direccionesdespacho.tid = tiendas.tid;";
 
   #Se prepara y ejecuta la consulta. Se obtienen TODOS los resultados
 	$result = $db -> prepare($query);
