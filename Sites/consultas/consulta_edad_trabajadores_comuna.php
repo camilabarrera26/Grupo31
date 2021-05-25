2ab953ddb7aa06f6c9a06d4b869e19490acba058
CAMILA BARRERA <camila.barrera@uc.cl>
2021-05-24 23:14:38 -0400

    'web13'

--- Sites/consultas/consulta_edad_trabajadores_comuna.php
+++ Sites/consultas/consulta_edad_trabajadores_comuna.php
@@ -7,9 +7,35 @@
 
   #Se obtiene el valor del input del usuario
   $comuna = $_POST["comuna_tienda"];
+  echo $comuna;
+  $comuna = strtolower($comuna);
+  echo $comuna;
+  $comuna = trim($comuna);
+  echo $comuna;
+  $count = 0;
+  while $count == 0:
+    $count = 1;
+    if (strpos($comuna, "á")) {
+      $comuna = str_replace('á', 'a', $comuna);
+      $count = 0;
+    } elseif (strpos($comuna, "é")) {
+      $comuna = str_replace('é', 'e', $comuna);
+      $count = 0;
+    } elseif (strpos($comuna, "í")) {
+      $comuna = str_replace('í', 'i', $comuna);
+      $count = 0;
+    } elseif (strpos($comuna, "ó")) {
+      $comuna = str_replace('ó', 'o', $comuna);
+      $count = 0;
+    } elseif (strpos($comuna, "ú")) {
+      $comuna = str_replace('ú', 'u', $comuna);
+      $count = 0;
+    }
+  endwhile;
+  echo $comuna;
 
   #Se construye la consulta como un string
- 	$query = "SELECT comunas.comuna_cobertura, AVG(personal.edad) FROM comunas, tiendas, personaltienda, personal WHERE '$comuna' = comunas.comuna_cobertura AND comunas.did = tiendas.direccion AND tiendas.tid = personaltienda.tid AND personaltienda.eid = personal.eid GROUP BY comunas.comuna_cobertura;";
+ 	$query = "SELECT comunas.comuna_cobertura, AVG(personal.edad) FROM comunas, tiendas, personaltienda, personal WHERE '%%$comuna%%' = comunas.comuna_cobertura AND comunas.did = tiendas.direccion AND tiendas.tid = personaltienda.tid AND personaltienda.eid = personal.eid GROUP BY comunas.comuna_cobertura;";
   echo $query;
   #Se prepara y ejecuta la consulta. Se obtienen TODOS los resultados
 	$result = $db -> prepare($query);
