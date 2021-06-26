eb7d95ed311c0916bf6bc1a60228f8e7ca4fe9be
CAMILA BARRERA <camila.barrera@uc.cl>
2021-06-26 11:23:20 -0400

    pagina compras 63

--- Sites/consultas/consulta_tienda.php
+++ Sites/consultas/consulta_tienda.php
@@ -5,8 +5,8 @@
     $nombre = $_REQUEST['nombre']; 
 ?>
 
-<div class="container px-lg-5">
-  <div class="p-4 p-lg-5 bg-light rounded-3 text-center">
+<div class="container px-lg-4">
+  <div class="p-4 p-lg-5 bg-body1 rounded-3 text-center">
     <div class="m-4 m-lg-5">
       <h1 class="display-5 fw-bold">Consulte por los 3 productos comestibles y no comestibles más económicos de la tienda:</h1>
         <?php
