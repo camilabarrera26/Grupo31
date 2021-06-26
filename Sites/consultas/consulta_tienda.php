48b09f8d0cd800ea9d1de43e661e69f40a2797b3
CAMILA BARRERA <camila.barrera@uc.cl>
2021-06-26 12:04:44 -0400

    pagina compras 73

--- Sites/consultas/consulta_tienda.php
+++ Sites/consultas/consulta_tienda.php
@@ -15,7 +15,7 @@
     <div class="m-4 m-lg-4">
       <h1 class="display-6 fw-bold">Consulte por los 3 productos comestibles y no comestibles más económicos de la tienda:</h1>
         <?php
-          echo "<a href='consulta_comestible_nocomestible_barato.php?id=$id&nombre=$nombre' role='button' class='btn'> Consulta </a>";
+          echo "<a href='consulta_comestible_nocomestible_barato.php?id=$id&nombre=$nombre' role='button' class='btn-secondary'> Consulta </a>";
         ?>
     </div>
   </div>
