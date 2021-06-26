68849a48ed4e8decc10f4f36c2b519846026cea6
CAMILA BARRERA <camila.barrera@uc.cl>
2021-06-26 12:16:31 -0400

    pagina compras 76

--- Sites/consultas/consulta_tienda.php
+++ Sites/consultas/consulta_tienda.php
@@ -24,7 +24,7 @@
 <div class="container-xl px-lg-4">
   <div class="p-4 p-lg-4 bg-primary rounded-3 text-center">
     <div class="m-4 m-lg-4">
-      Busque algún producto por nombre:
+      <h1 class="display-6 fw-bold">Busque algún producto por nombre:</h1>
       <form action="consulta_productos_vendidos.php" method="post">
         Producto:
         <input type="text" name="producto">
@@ -47,21 +47,25 @@ $result -> execute();
 $pid = $result -> fetchAll();
 ?>
 
-<br>
-Seleccione el ID del producto que desea comprar:
-<form action="consulta_compra.php" method="post">
-    ID Producto:
-    <select>
-    <?php
-    foreach ($pid as $p) {
-        echo "<option value=$p[0]>$p[0]</option>";
-    }
-    ?>
-    </select>
-  <input type="hidden" name="id_tienda" value="<?php echo $id; ?>">
-  <input type="submit" value="Comprar">
-</form>
-</br>
+<div class="container-xl px-lg-4">
+  <div class="p-4 p-lg-4 bg-primary rounded-3 text-center">
+    <div class="m-4 m-lg-4">
+      <h1 class="display-6 fw-bold">Seleccione el ID del producto que desea comprar:</h1>
+      <form action="consulta_compra.php" method="post">
+        ID Producto:
+        <select>
+        <?php
+        foreach ($pid as $p) {
+            echo "<option value=$p[0]>$p[0]</option>";
+        }
+        ?>
+        </select>
+        <input type="hidden" name="id_tienda" value="<?php echo $id; ?>">
+          input type="submit" value="Comprar" class='btn'>
+      </form>
+    </div>
+  </div>
+</div>
 
 <form action="consulta_todas_tiendas.php" method="get">
     <input type="submit" value="Volver">
