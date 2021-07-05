572c1efd68b550a2a9ab78df17ef8643b7a7a2db
CRISTIAN NOVA <cristian.nova@uc.cl>
2021-07-05 07:38:08 -0400

    interfaces

---
+++ Sites/interfaz/head.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html" charset="utf-8" /> <!-- idioma españiol-->
+    <link rel="icon" type="img/png" href="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSwkOhDMGyFVQOdJ5KsB0u49Msa4xDeTF0-dw&usqp=CAU">
+    <title> Delivery </title>
+    <link rel="stylesheet" type="text/css" href="tem.css">
+</head>
---
+++ Sites/interfaz/registro.php
@@ -0,0 +1,18 @@
+<?php include("head.php"); ?>
+
+<body>
+    <form action='./queries/procedimiento_registro.php' method='POST'>
+        <h1>Registrate Ahora</h1> <br>
+        <p>Nombre<br><input type="text" name="nombre" placeholder="Ingrese su nombre"></p> <br>
+        <p>Rut<br><input type="text" name="rut" placeholder="Ingrese su rut"></p> <br>
+        
+        <p>Sexo<br><select name='sexo' id='type'>
+                    <option value='hombre'>Hombre</option>
+                     <option value='mujer'>Mujer</option></select></p><br>
+
+        <p>Edad<br><input type='number' name='edad' placeholder="Ingrese su edad"></p> <br>
+        <p>Dirección<br><input type='text' name='direccion' placeholder="Ingrese su dirección"></p> <br>
+        <input class = "reg" type='submit' value='Registrarse'>
+    </form>
+</body>
+</html>
