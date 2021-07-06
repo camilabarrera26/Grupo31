6e187494ccdedf223817c8e5d6cd6d0c4461d817
CAMILA BARRERA <camila.barrera@uc.cl>
2021-07-05 20:57:36 -0400

    usarios 3

--- Sites/queries/procedimiento_entrada.php
+++ Sites/queries/procedimiento_entrada.php
@@ -14,6 +14,13 @@
     $result = $dbimp -> prepare($query);
     $result -> execute();
 
+    $query1 = "SELECT usuarios.uid FROM usuarios WHERE usuarios.rut = '$_POST[rut]';";
+    $id_usuario = $dbimp -> prepare($query);
+    $id_usuario -> execute();
+    echo $id_usuario[0];
+    echo $id_usuario;
+    print_r ($id_usuario);
+
     // Si nos interesa acceder a los booleanos que retorna el procedimiento, debemos hacer fetch de los resultados
     $personals = $result -> fetchAll();
     
