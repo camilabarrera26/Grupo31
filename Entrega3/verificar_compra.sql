13d280ab0af5b4d94c0274933cc4b5a9669c978f
CAMILA BARRERA <camila.barrera@uc.cl>
2021-07-06 12:17:18 -0400

    verificar compras 20

--- Entrega3/verificar_compra.sql
+++ Entrega3/verificar_compra.sql
@@ -12,7 +12,7 @@ RETURNS BOOLEAN AS $$
 DECLARE
 idmax int;
 a text;
-c cursor;
+
 
 -- definimos nuestra función
 BEGIN
@@ -22,16 +22,32 @@ BEGIN
         RETURN FALSE;
     END IF;
 
-    -- verificamos que la tienda despache a la comuna
     a := FALSE;
-    FOR c in (SELECT comunas.comuna_cobertura FROM usuarios, direccionesusuarios, comunas WHERE usuarios.uid = direccionesusuarios.uid AND direccionesusuarios.did = comunas.did AND uid_ = usuarios.uid) LOOP
-        IF c IN (SELECT DISTINCT comunas.comuna_cobertura FROM productos, productostiendas, tiendas, direccionesdespacho, comunas WHERE productos.pid = productostiendas.pid AND productostiendas.tid = tiendas.tid AND tiendas.tid = direccionesdespacho.tid AND direccionesdespacho.did = comunas.did AND productos.pid = pid_ AND tiendas.tid = tid_) THEN
-            a := TRUE;
-        END IF;
-    END LOOP;
-    IF a = FALSE THEN
-        RETURN FALSE;
+    DECLARE comuna text;
+    DECLARE c cursor for 
+    SELECT comunas.comuna_cobertura FROM usuarios, direccionesusuarios, comunas WHERE usuarios.uid = direccionesusuarios.uid AND direccionesusuarios.did = comunas.did AND uid_ = usuarios.uid;
+
+    OPEN c
+
+    FETCH NEXT FROM c INTO comuna
+
+    WHILE @@FETCH_STATUS = 0
+    BEGIN
+    
+    IF c IN (SELECT DISTINCT comunas.comuna_cobertura FROM productos, productostiendas, tiendas, direccionesdespacho, comunas WHERE productos.pid = productostiendas.pid AND productostiendas.tid = tiendas.tid AND tiendas.tid = direccionesdespacho.tid AND direccionesdespacho.did = comunas.did AND productos.pid = pid_ AND tiendas.tid = tid_) THEN
+        a := TRUE;
     END IF;
+
+    FETCH NEXT FROM c INTO comuna
+    END
+
+CLOSE c
+DEALLOCATE c
+
+IF a = FALSE THEN
+    RETURN FALSE;
+END IF;
+
 
     -- insertamos el maximo id en la variable idmax
     SELECT INTO idmax
