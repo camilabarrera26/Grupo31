711a8049a5d386b13891daebba0185c5a589526e
CAMILA BARRERA <camila.barrera@uc.cl>
2021-07-06 00:29:10 -0400

    usarios 48

--- Sites/queries/procedimiento_registro.php
+++ Sites/queries/procedimiento_registro.php
@@ -2,16 +2,12 @@
 <html>
 
 <?php
-    // Nos conectamos a las bdds
     require("../config/conexion.php");
 
-    // Enviamos del post la informacion a la query con nuestro procedimiento almacenado que realizará
-    // las verificaciones correspondientes
     $query = "SELECT registrar_usuario('$_POST[nombre]', '$_POST[rut]', '$_POST[sexo]', $_POST[edad], '$_POST[direccion]');";
     $result = $dbimp -> prepare($query);
     $result -> execute();
 
-    // Si nos interesa acceder a los booleanos que retorna el procedimiento, debemos hacer fetch de los resultados
     $personals = $result -> fetchAll();
     $a = $personals['0'];
 
