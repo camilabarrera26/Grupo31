676c5b6f104114094dfad07c42b3148a3d6a8fcd
CAMILA BARRERA <camila.barrera@uc.cl>
2021-07-05 23:14:15 -0400

    usarios 30

--- Sites/index.php
+++ Sites/index.php
@@ -20,6 +20,8 @@
 Welcome <?php //echo $_SESSION["name"]; ?>. Click here to <a href="logout.php" tite="Logout">Logout.
 <?php 
 //}else echo "<h1>Please login first .</h1>";
+
+echo $_SESSION['id'];
 ?>
 -->
 
