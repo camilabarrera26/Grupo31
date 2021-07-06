017914df477990166e0551051b7ebba2f9bbb4c8
CAMILA BARRERA <camila.barrera@uc.cl>
2021-07-05 23:43:21 -0400

    usarios 38

--- Sites/consultas/consulta_todas_tiendas.php
+++ Sites/consultas/consulta_todas_tiendas.php
@@ -2,7 +2,7 @@
 session_start();
 if(!isset($_SESSION['rut'])) // If session is not set then redirect to Login Page
        {
-           header("Location:login.php");  
+           header("Location: ../login.php");  
        }
 ?>
 
