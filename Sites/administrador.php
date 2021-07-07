49bc524d264a7299d3a6584cb9f5ceb63e2f7573
Rocio Apablaza <rapablaza@uc.cl>
2021-07-07 04:17:28 -0400

    Update administrador.php

--- Sites/administrador.php
+++ Sites/administrador.php
@@ -1,20 +1,16 @@
 <?php
-    require("config/conexion.php");
-    $message="";
-    if(count($_POST)>0) {
-        $contrasena = $_POST["contrasena"];
+  #Llama a conexión, crea el objeto PDO y obtiene la variable $db
+  require("config/conexion.php");
+#Se construye la consulta como un string
+  $query = "SELECT * FROM usuarios ORDER BY usuarios.uid;";
 
-        if ($contrasena = "administrador123") {
-            $query1 = "SELECT * FROM usuarios ORDER BY usuarios.uid';";
-            $result1 = $dbimp -> prepare($query1);
-            $result1 -> execute();
-            $usuario = $result1 -> fetchAll();
-        } else {
-            $message = "Error al Entrar!";
-        } 
+  #Se prepara y ejecuta la consulta. Se obtienen TODOS los resultados
+  $result = $dbimp -> prepare($query);
+  $result -> execute();
+  $usuario = $result -> fetchAll();
 
 ?>
-<body>  
+
   <table class='table'>
     <tr>
       <th>Nombre</th>
@@ -31,33 +27,3 @@
       }
       ?>
   </table>
-
-<body>  
-
-
-<?php include('templates/header.html');   ?>
-
-<header class="py-5">
-    <div class="container px-lg-0">
-        <div class="p-4 p-lg-2 bg-light rounded-3 text-center">
-            <div class="m-4 m-lg-2">
-                <h1 class="display-5 fw-bold">Mi Tienda Web</h1> 
-                 <p class="fs-4">Regístrate</p>  
-            </div>
-        </div>
-    </div>
-</header>
-
-<body>
-<form method='POST' name="frmUser" action="" align="center">
-<div class="message"><?php if($message!="") { echo $message; } ?></div>
-<h3 align="center">Ingresa tus datos de administrador:</h3>
- Contrasena:<br>
- <input type="text" name="contrasena">
- <br>
-
-<input type="submit" name="submit" value="Submit">
-<input type="reset">
-</form>
-</body>
-</html>
