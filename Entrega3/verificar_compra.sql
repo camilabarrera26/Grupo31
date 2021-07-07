b92570bff280dcc89a9f5fbf15b03b0bdc6dc760
CAMILA BARRERA <camila.barrera@uc.cl>
2021-07-06 20:46:43 -0400

    cambio de contrasena 17

--- Entrega3/verificar_compra.sql
+++ Entrega3/verificar_compra.sql
@@ -17,13 +17,11 @@ idmax1 int;
 BEGIN
 
     -- si el producto no esta en la tienda retorna false
-    IF pid_ NOT IN (SELECT DISTINCT productos.pid FROM productos, productostiendas, tiendas WHERE productos.pid = productostiendas.pid AND productostiendas.tid = tiendas.tid AND productos.pid = pid_ AND tiendas.tid = tid_) THEN
-        RETURN FALSE;
-    END IF;
+
 
     -- verificamos que la tienda despache a la comuna
     IF comuna NOT IN (SELECT DISTINCT comunas.comuna_cobertura FROM productos, productostiendas, tiendas, direccionesdespacho, comunas WHERE productos.pid = productostiendas.pid AND productostiendas.tid = tiendas.tid AND tiendas.tid = direccionesdespacho.tid AND direccionesdespacho.did = comunas.did AND productos.pid = pid_ AND tiendas.tid = tid_) THEN
-        RETURN TRUE;
+        RETURN FALSE;
     END IF;
 
     -- insertamos el maximo id en la variable idmax
