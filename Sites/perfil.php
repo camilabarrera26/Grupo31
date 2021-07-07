07dfb9184b575942202bb9d3235145822ec2dd9c
CAMILA BARRERA <camila.barrera@uc.cl>
2021-07-07 18:11:49 -0400

    ultimos detalles 17

--- Sites/perfil.php
+++ Sites/perfil.php
@@ -72,33 +72,6 @@ session_start();
 </table>
 
 <?php
-
-$query5 = "SELECT verficar_jefe($id);";
-
-$result5 = $dbp -> prepare($query5);
-$result5 -> execute();
-$jefe = $result5 -> fetchAll();
-
-if ($jefe == true) {
-  echo "<h1> Administrativos en su Unidad </h1>";
-  $query6 = "SELECT rellenar con query que obtiene administrativos trabajando en $direccion;";
-
-  $result6 = $dbp -> prepare($query6);
-  $result6 -> execute();
-  $administrativos = $result6 -> fetchAll();
-  echo "<table class='table'>";
-  echo "<tr>";
-  echo "<th>Nombre administrativo</th>";
-  echo "</tr>";
-        foreach ($administrativos as $a) {
-                echo "<tr><td>$a[0]</td></tr>";
-          }
-  echo "</table>";
-} 
-
-?>
-
-<?php
   $pila = array();
   foreach ($fecha as $fe){
     foreach ($compra as $c){
@@ -140,26 +113,45 @@ if ($jefe == true) {
 </div>
 </div>
 
-<h1> Historial de Compras </h1>
-<table class='table'>
-  <tr>
-    <th>Nombre Tienda</th>
-    <th>Producto</th>
-    <th>Dirección de envío</th>
-    <th>Comuna de envío</th>
-    <th>Fecha de envío</th>
-  </tr>
-    <?php
-    foreach ($pila as $p){
-    echo $p;
-    }
-    ?>
-</table>
+<?php
 
-<p class="fs-4">Aquí podrás cambiar tu contraseña:
-  <a href='consultas/cambio_contrasena.php' role='button' class='btn'> Cambiar Contraseña </a>
-  </p>  
+$query5 = "SELECT verficar_jefe($id);";
+
+$result5 = $dbp -> prepare($query5);
+$result5 -> execute();
+$jefe = $result5 -> fetchAll();
+
+if ($jefe == true) {
+  echo "<h1> Administrativos en su Unidad </h1>";
+  $query6 = "SELECT rellenar con query que obtiene administrativos trabajando en $direccion;";
+
+  $result6 = $dbp -> prepare($query6);
+  $result6 -> execute();
+  $administrativos = $result6 -> fetchAll();
+  echo "<table class='table'>";
+  echo "<tr>";
+  echo "<th>Nombre administrativo</th>";
+  echo "</tr>";
+        foreach ($administrativos as $a) {
+                echo "<tr><td>$a[0]</td></tr>";
+          }
+  echo "</table>";
+} 
+?>
+
+<div class='py-5'>
+<div class="container-xl px-lg-4">
+  <div class="p-4 p-lg-4 bg-primary rounded-3 text-center">
+    <div class="m-4 m-lg-4">
+      <h1 class="display-6 fw-bold">Consulte por los 3 productos comestibles y no comestibles más económicos de la tienda:</h1>
+        <p class="fs-4">Aquí podrás cambiar tu contraseña:
+          <a href='consultas/cambio_contrasena.php' role='button' class='btn'> Cambiar Contraseña </a>
+        </p>  
+    </div>
+  </div>
+</div>
+</div>   
+
+
 
 <a href='index.php' role='button' class='btn'> Volver </a>
-
-    
